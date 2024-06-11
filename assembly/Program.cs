cce3c2b8a4935e1936d4fbda10457cedc1eea402
nebucanetshar <alhannb@hotmail.com>
2024-06-11 16:51:11 +0200

    none solution find for runtime to incompatibility

--- assembly/Program.cs
+++ assembly/Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using assembly;
+using System;
+using System.Diagnostics;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,3 +12,18 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
+
+// chemin ver l'executable non autonnome 
+string grpc = @"..\common\common.csproj";
+
+// lancer l'éxécutable non autonome 
+ProcessStartInfo processStartInfo = new ProcessStartInfo(grpc);
+Process process = Process.Start(processStartInfo);
+
+//attendre que le processus se termine
+process.WaitForExit();
+
+//Récupérer le code de sortie du processus 
+int exitCode = process.ExitCode;
+
+
---
+++ common/ViewModel/ViewModelBase.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+
+namespace common.ViewModel;
+
+public class ViewModelBase
+{
+    #region Attributes & Accessors
+    #endregion
+
+    #region Constructors
+    #endregion
+
+    #region Methods
+    public string ToStringJson()
+    {
+        return JsonSerializer.Serialize(this);
+    }
+    #endregion
+}
