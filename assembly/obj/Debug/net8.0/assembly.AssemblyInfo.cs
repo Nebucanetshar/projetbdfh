7cb03059e8550ed1785d23c6163410aa52257ddb
nebucanetshar <alhannb@hotmail.com>
2024-07-01 15:31:33 +0200

    add package and directory fluxor

--- assembly/obj/Debug/net8.0/assembly.AssemblyInfo.cs
+++ assembly/obj/Debug/net8.0/assembly.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("assembly")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f906327769b9b2b8cc930c568ebb0615378043be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("assembly")]
 [assembly: System.Reflection.AssemblyTitleAttribute("assembly")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
